be7b87f400aabb3d9043c7f7f21f0ff01f459f26
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2024-03-12 11:33:37 -0300

    error vercel test

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,52 @@
+
+
+
+
+
+import styled from "styled-components";
+
+import backgroundImg from "../../assets/devs.png"
+
+export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    align-items: stretch;
+`
+export const Form = styled.form`
+    padding: 0 136px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    text-align: center;
+
+    >h1{
+        font-size: 48px;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+    }
+
+    >h2{
+        font-size: 24px;
+        margin-top: 84px;
+        margin-bottom: 24px;
+    }
+
+    >p{
+        font-size: 14px;
+        color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+
+    >a{
+        margin-top: 124px;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+    }
+`
+
+export const Background = styled.div`
+    flex: 1;
+    background: url(${backgroundImg}) no-repeat center center;
+    background-size: cover;
+`
