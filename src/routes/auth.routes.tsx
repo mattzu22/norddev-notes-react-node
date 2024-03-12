2b596f4be0f484191beaed3f5c5a484ab3168292
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2024-03-12 14:48:01 -0300

    error vercel teste

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,14 +1,14 @@
 
 import { Routes, Route } from "react-router-dom"
 
-import { SignIn } from "../pages/Signin" 
+import { SignIn } from "../pages/SignIn" 
 import { SignUp } from "../pages/SignUp"
 
 export function AuthRoutes(){
     return (
         <Routes>
-         <Route path="/register" element={<SignUp />}/>
-         <Route path="/" element={<SignIn />}/>
+         <Route path="/" element={<SignIn />} />
+         <Route path="/register" element={<SignUp />} />
         </Routes>
     )
 }
